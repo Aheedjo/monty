d417e7e99f77d2b26d4d127fb4e4a662399da5f5
lami_lekan <olagbajuisrael11@gmail.com>
2023-12-14 20:30:05 +0000

    pint func

---
+++ pint.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+/**
+* f_pint - prints last stack entry
+* @head: stack head
+* @counter: line_number
+* Return: Always 0
+*/
+
+void f_pint(stack_t **head, unsigned int counter)
+{
+    if (*head == NULL)
+    {
+        fprintf(stderr, "L%u: can't pint, stack empty\n",counter);
+        f.close(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+    printf("%d\n", (*head)->n);
+}
